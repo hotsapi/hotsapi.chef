fe5ad0b36357c871849374f7ed5c3c8a3132504b
Poma <Semenov.Roman@mail.ru>
2017-08-27 19:27:11 +0300

    php.ini

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,7 @@
-package %w(php7.0 php7.0-fpm php7.0-mysql php7.0-zip php7.0-gd mcrypt php7.0-mcrypt php7.0-mbstring php7.0-xml php7.0-curl php7.0-json composer zip unzip nginx git)
+package %w(php7.0 php7.0-fpm php7.0-mysql php7.0-zip php7.0-gd mcrypt php7.0-mcrypt php7.0-mbstring php7.0-xml php7.0-curl php7.0-json composer zip unzip nginx memcache php-memcached redis-server git)
 
 service 'nginx'
+service 'php7.0-fpm'
 
 git '/var/www/hotsapi' do
   repository "https://github.com/poma/hotsapi.git"
@@ -10,6 +11,10 @@ end
 cookbook_file '/etc/nginx/sites-available/default' do
   source "nginx.conf"
   notifies :restart, "service[nginx]"
+end
+
+cookbook_file '/etc/php/7.0/fpm/php.ini' do
+  notifies :restart, "service[php7.0-fpm]"
 end
 
 template '/var/www/hotsapi/.env' do
@@ -38,6 +43,7 @@ link '/usr/bin/heroprotocol' do
   to '/opt/heroprotocol/heroprotocol.py'
 end
 
+# for some reason file mode can be broken
 file '/opt/heroprotocol/heroprotocol.py' do
   mode 755
 end
