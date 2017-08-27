8a870364d8626ea4c7d4ca12fa53438d20a65804
Poma <Semenov.Roman@mail.ru>
2017-08-27 17:42:02 +0300

    don't install chef twice

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-apt install -y git wget
-wget https://packages.chef.io/files/stable/chefdk/2.1.11/ubuntu/16.04/chefdk_2.1.11-1_amd64.deb
-dpkg -i chefdk_2.1.11-1_amd64.deb
+apt install -y git curl
+if ! type berks > /dev/null; then
+    if ! -f chefdk.deb; then
+        curl https://packages.chef.io/files/stable/chefdk/2.1.11/ubuntu/16.04/chefdk_2.1.11-1_amd64.deb > chefdk.deb
+    fi
+    dpkg -i chefdk.deb
+fi
 berks vendor ./cookbooks
 chef-client -z -j $(pwd)/chef.json
