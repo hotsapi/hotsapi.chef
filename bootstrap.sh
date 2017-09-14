7194ce6d2d967ba6bb81bee13cf3e7e992649a6b
Roman Semenov <Semenov.Roman@mail.ru>
2017-09-14 12:23:10 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 apt install -y git curl
 if ! type berks > /dev/null; then
     if ! -f chefdk.deb; then
-        curl https://packages.chef.io/files/stable/chefdk/2.1.11/ubuntu/16.04/chefdk_2.1.11-1_amd64.deb > chefdk.deb
+        curl https://packages.chef.io/files/stable/chefdk/2.2.1/ubuntu/16.04/chefdk_2.2.1-1_amd64.deb > chefdk.deb
     fi
     dpkg -i chefdk.deb
 fi
