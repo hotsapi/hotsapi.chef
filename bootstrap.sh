97e8b7b64dc34ddbbfa291cb98308640b6e0bb8a
Roman Semenov <Semenov.Roman@mail.ru>
2017-09-16 11:34:31 +0300

    install chef from omnibus

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 apt install -y git curl
 if ! type berks > /dev/null; then
-    if ! -f chefdk.deb; then
-        curl https://packages.chef.io/files/stable/chefdk/2.2.1/ubuntu/16.04/chefdk_2.2.1-1_amd64.deb > chefdk.deb
-    fi
-    dpkg -i chefdk.deb
+    curl -L https://omnitruck.chef.io/install.sh | sudo bash -s -- -P chefdk
 fi
 berks vendor ./cookbooks
 chef-client -z -j $(pwd)/chef.json
