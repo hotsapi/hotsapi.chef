8a7397b4251494ce72cf4ab11b955bc3223e2e54
Roman Semenov <Semenov.Roman@mail.ru>
2017-09-16 11:54:59 +0300

    root check

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
 apt install -y git curl
 if ! type berks > /dev/null; then
     curl -L https://omnitruck.chef.io/install.sh | sudo bash -s -- -P chefdk
