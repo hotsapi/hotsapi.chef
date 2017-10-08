b080165762672aef0a0229d97740d7875357aaf4
Poma <Semenov.Roman@mail.ru>
2017-10-08 03:43:26 +0300

    cloudwatch logs

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,35 @@
 default['hotsapi']['install_mysql'] = false
+
+default['cwlogs']['region'] = 'eu-west-1'
+
+default['cwlogs']['logfiles']['access-log'] = {
+  :log_stream_name  => '{instance_id}',
+  :log_group_name   => 'nginx/access.log',
+  :file             => '/var/log/nginx/access.log',
+  :datetime_format  => '%Y-%m-%d %H:%M:%S',
+  :initial_position => 'end_of_file'
+}
+
+default['cwlogs']['logfiles']['error-log'] = {
+  :log_stream_name  => '{instance_id}',
+  :log_group_name   => 'nginx/error.log',
+  :file             => '/var/log/nginx/error.log',
+  :datetime_format  => '%Y-%m-%d %H:%M:%S',
+  :initial_position => 'end_of_file'
+}
+
+default['cwlogs']['logfiles']['laravel-log'] = {
+  :log_stream_name  => '{instance_id}',
+  :log_group_name   => 'laravel/laravel.log',
+  :file             => '/var/www/hotsapi/storage/logs/laravel.log',
+  :datetime_format  => '%Y-%m-%d %H:%M:%S',
+  :initial_position => 'end_of_file'
+}
+
+default['cwlogs']['logfiles']['worker-log'] = {
+  :log_stream_name  => '{instance_id}',
+  :log_group_name   => 'laravel/worker.log',
+  :file             => '/var/www/hotsapi/storage/logs/worker.log',
+  :datetime_format  => '%Y-%m-%d %H:%M:%S',
+  :initial_position => 'end_of_file'
+}
